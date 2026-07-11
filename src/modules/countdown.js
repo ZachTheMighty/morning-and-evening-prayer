8de4ffed99fbbe9920fecc7c55cbb8fbd6c42ba5
ZachTheMighty <promoted66@gmail.com>
2026-07-11 11:01:55 +0300

    fix: coundown() not counting the last second in a minute and the last minute in an hour

--- src/modules/countdown.js
+++ src/modules/countdown.js
@@ -4,16 +4,17 @@ import updateDOM from "./update_dom.js";
 export default async function (hours, minutes, seconds, prayer) {
   while (true) {
     if (hours === 0 && minutes === 0 && seconds === 0) timeRemaining();
+
     seconds--;
-    if (seconds === 0 && minutes > 0) {
+    if (seconds < 0) seconds = 59;
+
+    if (seconds === 59 && minutes > 0) {
       minutes--;
-      seconds = 59;
+      if (minutes < 0) minutes = 59;
     }
 
-    if (minutes === 0 && hours > 0) {
-      hours--;
-      minutes = 59;
-    }
+    if (minutes === 0 && hours > 0) hours--;
+
     updateDOM(hours, minutes, seconds, prayer);
     await delay(1);
   }
